25233c22397d792c928d3c377eeb02300372c9ad
Fardin Kashef <fardinkashef1397@gmail.com>
2025-04-12 22:44:28 +0330

    Add different MongoDB urls to .env file for development (to use mongodb locally on my machine) and production (when deploying my project on vercel and using mongodCb atlas). Also Change connectToDatabase function logic to connect to proper database based on the current environment (development or production)

--- funguage-next-version/src/lib/database/db-connection.ts
+++ funguage-next-version/src/lib/database/db-connection.ts
@@ -5,23 +5,30 @@ let isConnected: boolean = false;
 export const connectToDatabase = async () => {
   // mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URL) {
-    return console.log("MISSING MONGODB_URL");
-  }
-
   if (isConnected) {
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      // dbName: "funguage",
-      dbName: "funguage-local",
-      enableUtf8Validation: false,
-    });
+    if (process.env.NODE_ENV === "development") {
+      if (!process.env.MONGODB_LOCAL_URL)
+        return console.log("MISSING MONGODB_LOCAL_URL");
+
+      await mongoose.connect(process.env.MONGODB_LOCAL_URL, {
+        dbName: "funguage-local",
+        enableUtf8Validation: false,
+      });
+    } else if (process.env.NODE_ENV === "production") {
+      if (!process.env.MONGODB_ATLAS_URL)
+        return console.log("MISSING MONGODB_ATLAS_URL");
+
+      await mongoose.connect(process.env.MONGODB_ATLAS_URL, {
+        dbName: "funguage",
+        enableUtf8Validation: false,
+      });
+    }
 
     isConnected = true;
-
     console.log("MongoDB is connected");
   } catch (error) {
     console.log("MongoDB connection failed", error);
